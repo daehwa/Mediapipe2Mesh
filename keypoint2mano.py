1e740b2ceb18b785353e2f80bca2166dc1a485c9
daehwa <daehwak@GS22394.SP.CS.CMU.EDU>
2024-07-02 03:53:03 -0700

    first commit

---
+++ keypoint2mano.py
@@ -0,0 +1,74 @@
+import sys
+import numpy as np
+import argparse
+from inverse_kinematics.solver import *
+from inverse_kinematics.armatures import *
+from inverse_kinematics.models import *
+import inverse_kinematics.config
+from scipy.spatial.transform import Rotation as R
+import glob
+
+n_pose = 17
+
+class Keypoints2Mano():
+    def __init__(self,model_path='./MANO_RIGHT.npz'):
+        mesh = KinematicModel(model_path, MANOArmature, scale=1000)
+        self.wrapper = KinematicPCAWrapper(mesh, n_pose=n_pose)
+        self.solver = Solver(max_iter=3,verbose=True)
+        self.mesh = mesh
+
+    def get_mano_params(self, keypoints):
+        keypoints = self.to_wrist_coordinate(keypoints)
+        keypoints = self.mediapipe2mano_joints(keypoints)
+        keypoints = self.set_default_rotation(keypoints)
+        params_est = self.solver.solve(self.wrapper, keypoints, u=35, v=10)
+        shape_est, pose_pca_est, pose_glb_est = self.wrapper.decode(params_est)
+        self.set_mesh_params(pose_pca_est,
+                             np.zeros(pose_glb_est.shape),
+                             np.zeros(shape_est.shape))
+        return pose_pca_est
+
+    def to_wrist_coordinate(self,hand_data):
+        def normalize(x):
+            res = x / np.linalg.norm(x)
+            return res
+        v05 = hand_data[5]-hand_data[0]
+        print(np.linalg.norm(v05))
+        v017 = hand_data[17]-hand_data[0]
+        normal = normalize(np.cross(v017,v05))
+        x_axis = normalize(v05)
+        y_axis = normalize(normal)
+        z_axis = normalize(np.cross(x_axis,y_axis))
+        for i in range(len(hand_data)):
+            v = np.copy(hand_data[i])
+            hand_data[i, 0] = np.dot(v,x_axis)
+            hand_data[i, 1] = np.dot(v,y_axis)
+            hand_data[i, 2] = np.dot(v,z_axis)
+        hand_data[:, 2] *= -1
+        return hand_data
+
+    def set_mesh_params(self,pose_pca,pose_glb,shape):
+        self.mesh.set_params(pose_pca=pose_pca, pose_glb=pose_glb, shape=shape)
+
+    def get_vertices(self):
+        return self.mesh.verts
+
+    def get_faces(self):
+        return self.mesh.faces
+
+    def mediapipe2mano_joints(self,keypoints):
+        mapping = [0,13,14,15,20,1,2,3,16,4,5,6,17,10,11,12,19,7,8,9,18]
+        mano_keypoints = np.zeros((21,3))
+        for i, m in enumerate(mapping):
+            mano_keypoints[m] = keypoints[i]
+        return mano_keypoints
+
+    def set_default_rotation(self,keypoints):
+        keypoints = keypoints*1.85*500
+        r_mat = R.from_euler('xyz', [180,190,0], degrees=True).as_matrix()
+        origin = np.array([95.66993092,6.38342886,6.18630528])
+        for i in range(keypoints.shape[0]):
+            k = np.array([keypoints[i]]).T
+            keypoints[i] = np.matmul(r_mat,k).T[0]
+        keypoints = keypoints - (keypoints[0]-origin)
+        return keypoints
---
+++ main.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+
+## ------------------- ##
+## Inverse Kinematics  ##
+## ------------------- ##
+from keypoint2mano import Keypoints2Mano
+from inverse_kinematics.armatures import *
+from inverse_kinematics.models import *
+k2m = Keypoints2Mano(model_path='./MANO_RIGHT.npz')
+
+## ------------------- ##
+##      Visualizer     ##
+## ------------------- ##
+import open3d as o3d
+
+mesh = o3d.geometry.TriangleMesh()
+verts = k2m.get_vertices()
+faces = k2m.get_faces()
+mesh.vertices = o3d.utility.Vector3dVector(verts)
+mesh.triangles = o3d.utility.Vector3iVector(faces)
+mesh.compute_vertex_normals()
+mesh.paint_uniform_color([0.9, 0.8, 0.7])
+
+vis = o3d.visualization.Visualizer()
+vis.create_window(window_name='EtherPose Viewer')
+vis.add_geometry(mesh)
+
+## ------------------- ##
+##      Mediapipe      ##
+## ------------------- ##
+import mediapipe as mp
+
+# Initialize MediaPipe hands module
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, 
+                        model_complexity=1,
+                        max_num_hands=1,
+                        min_detection_confidence=0.5,
+                        min_tracking_confidence=0.5)
+
+# Initialize MediaPipe drawing module
+mp_drawing = mp.solutions.drawing_utils
+
+# Open video stream (0 for the default camera, or replace with video file path)
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Convert the frame to RGB
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Process the frame to find hand landmarks
+    results = hands.process(image_rgb)
+
+    # Draw the hand annotations on the frame
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_world_landmarks:
+            hand_landmarks_array = np.array([[lmk.x, lmk.y, lmk.z] for lmk in hand_landmarks.landmark])
+            pose_hand = k2m.get_mano_params(hand_landmarks_array)
+            verts = k2m.get_vertices()
+            faces = k2m.get_faces()
+
+            mesh.vertices = o3d.utility.Vector3dVector(verts)
+            mesh.triangles = o3d.utility.Vector3iVector(faces)
+            mesh.compute_vertex_normals()
+            vis.update_geometry(mesh)
+
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    vis.poll_events()
+    vis.update_renderer()
+
+    # Display the frame
+    cv2.imshow('MediaPipe Hands', frame)
+
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
+
+# Release resources
+cap.release()
+cv2.destroyAllWindows()
+hands.close()
